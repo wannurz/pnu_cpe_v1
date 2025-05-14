568ab6cfb15f4a7791d0bc3895c9d6549226963b
wannurz <6560506007@pnu.ac.th>
2025-05-14 11:45:07 +0700

    Update main.blocks, pnu_cpe.ts

--- pnu_cpe.ts
+++ pnu_cpe.ts
@@ -344,7 +344,7 @@ namespace cpe_pnu {
     //% blockId="cpe_writeDigital" block="Digital Write pin %pin|value %value"
     //% value.min=0 value.max=1
     //% weight=55
-    //% group="Digital In/Out"
+    //% group="Digital Input/Output"
     export function cpe_writeDigital(pin: cpeDigitalPin, value: number): void {
         pins.digitalWritePin(pin, value)
     }
@@ -354,7 +354,7 @@ namespace cpe_pnu {
      */
     //% blockId="cpe_readDigital" block="Digital Read pin %pin"
     //% weight=50
-    //% group="Digital In/Out"
+    //% group="Digital Input/Output"
     export function cpe_readDigital(pin: cpeDigitalPin): number {
         return pins.digitalReadPin(pin)
     }
@@ -364,7 +364,7 @@ namespace cpe_pnu {
  */
     //% blockId="cpe_readAnalog" block="Read analog pin %pin"
     //% weight=50
-    //% group="Analog I/O"
+    //% group="Analog Input/Output"
     export function readAnalog(pin: cpeAnalogPin): number {
         switch (pin) {
             case cpeAnalogPin.P0: return pins.analogReadPin(AnalogPin.P0);
@@ -390,7 +390,7 @@ namespace cpe_pnu {
      */
     //% blockId="cpe_analogWrite" block="Analog write %value|to pin %pin"
     //% weight=49
-    //% group="Analog I/O"
+    //% group="Analog Input/Output"
     export function analogWrite(pin: cpeAnalogPin, value: number): void {
         value = Math.clamp(0, 1023, value);
         switch (pin) {
